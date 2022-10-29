400c56bf9d11000898103e90d5ca3d9b7264998f
kljc <614553985@qq.com>
2022-10-30 03:03:03 +0800

    test2

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -17,7 +17,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import sun.invoke.util.Wrapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
